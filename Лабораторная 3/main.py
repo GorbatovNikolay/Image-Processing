832c6ffe588a03e089c27703bfbbd6291eb7a42e
Nikolay Gorbatov <gorb.koly2000@gmail.com>
2023-05-17 15:08:05 +0300

    Lab 3 added.

---
+++ Лабораторная 3/main.py
@@ -0,0 +1,49 @@
+import os
+
+from imageai.Detection import ObjectDetection, VideoObjectDetection
+
+EXEC_PATH = os.getcwd()
+
+
+def detect_objects():
+    detector = ObjectDetection()
+    detector.useCPU()
+
+    detector.setModelTypeAsRetinaNet()
+    detector.setModelPath(os.path.join(EXEC_PATH, 'retinanet_resnet50_fpn_coco-eeacb38b.pth'))
+    detector.loadModel()
+
+    objects_from_image = detector.detectObjectsFromImage(
+        input_image=os.path.join(EXEC_PATH, 'people.jpg'),
+        output_image_path=os.path.join(EXEC_PATH, 'processed_people.jpg'),
+        minimum_percentage_probability=20,
+        display_percentage_probability=True,
+        display_object_name=True
+    )
+
+
+def detect_video_objects():
+    detector = VideoObjectDetection()
+    detector.useCPU()
+
+    detector.setModelTypeAsYOLOv3()
+    detector.setModelPath(os.path.join(EXEC_PATH, 'yolov3.pt'))
+    detector.loadModel()
+
+    frames = detector.detectObjectsFromVideo(
+        input_file_path=os.path.join(EXEC_PATH, 'traffic.mp4'),
+        output_file_path=os.path.join(EXEC_PATH, 'traffic_detected'),
+        frames_per_second=20,
+        log_progress=True
+    )
+
+    print(frames)
+
+
+def main():
+    # detect_objects()
+    detect_video_objects()
+
+
+if __name__ == '__main__':
+    main()
